9d2237fd88eb58f4fd75dd95834a762e8c949a8d
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-01 15:04:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/', // Garante que o caminho dos scripts seja sempre a raiz
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
+      build: {
+        cssCodeSplit: false, // Desativa a divisão de CSS já que você não usa arquivos CSS
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
