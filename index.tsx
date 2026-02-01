68c27f9bc65e5c62e02c1ca3571d0f3e0390f646
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-01 02:44:11 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-///import './index.css'; // ✅ OBRIGATÓRIO para o Tailwind funcionar
 
 const rootElement = document.getElementById('root');
-
 if (!rootElement) {
-  throw new Error('Root element not found');
+  throw new Error("Could not find root element to mount to");
 }
 
-ReactDOM.createRoot(rootElement).render(
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
